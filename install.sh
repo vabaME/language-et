89bd2a32908ea8da4e4a500fd617b05eaafeddae
vaba <92581921+vabaME@users.noreply.github.com>
2021-10-17 11:51:06 +0300

    gufw added

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#coreutils
+
 sudo install -Dm644  update-manager.mo /usr/share/locale-langpack/et/LC_MESSAGES/
 sudo install -Dm644  update-notifier.mo /usr/share/locale-langpack/et/LC_MESSAGES/
 sudo install -Dm644  apport.mo /usr/share/locale-langpack/et/LC_MESSAGES/
 sudo install -Dm644  systemd.mo /usr/share/locale-langpack/et/LC_MESSAGES/
 sudo install -Dm644  ufw.mo /usr/share/locale-langpack/et/LC_MESSAGES/
+sudo install -Dm644  gufw.mo /usr/share/locale/et/LC_MESSAGES/
 
 exit 1
