adadbe82b60c439d84671e56425fc8b13fe2710b
vaba <92581921+vabaME@users.noreply.github.com>
2021-10-21 15:52:17 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-#coreutils
+
 sudo install -Dm644  update-manager.mo /usr/share/locale-langpack/et/LC_MESSAGES/
 sudo install -Dm644  update-notifier.mo /usr/share/locale-langpack/et/LC_MESSAGES/
 sudo install -Dm644  apport.mo /usr/share/locale-langpack/et/LC_MESSAGES/
 sudo install -Dm644  systemd.mo /usr/share/locale-langpack/et/LC_MESSAGES/
 sudo install -Dm644  ufw.mo /usr/share/locale-langpack/et/LC_MESSAGES/
 sudo install -Dm644  gufw.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  synaptic.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  synaptic.mo /usr/share/locale-langpack/et/LC_MESSAGES/
+  
 
 exit 1
