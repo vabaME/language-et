30b08cac70ee40203edc3bf78ab3b1f4529687b0
vaba <92581921+vabaME@users.noreply.github.com>
2022-01-08 14:04:27 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#coreutils
+
+sudo install -Dm644  systemd.mo /usr/share/locale-langpack/et/LC_MESSAGES/
+sudo install -Dm644  ufw.mo /usr/share/locale-langpack/et/LC_MESSAGES/
+sudo install -Dm644  gufw.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  synaptic.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  libelf.mo /usr/share/locale-langpack/et/LC_MESSAGES/  
+sudo install -Dm644  bleachbit.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  file-roller.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  transmission-gtk.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  kazam.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  cinnamon.mo /usr/share/locale/et/LC_MESSAGES/
+exit 1
