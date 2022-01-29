edc83f189076a820a096d571dd477882d4182ded
vaba <92581921+vabaME@users.noreply.github.com>
2022-01-29 10:38:01 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -14,6 +14,6 @@ sudo install -Dm644  nm-applet.mo /usr/share/locale/et/LC_MESSAGES/
 sudo install -Dm644  gnome-terminal.mo /usr/share/locale/et/LC_MESSAGES/
 sudo install -Dm644  nm-connection-editor.desktop /usr/share/applications/
 sudo install -Dm644  gnome-calculator.mo /usr/share/locale/et/LC_MESSAGES/
-sudo install -Dm644  udisks2.mo /usr/share/locale/et/LC_MESSAGES/
-sudo install -Dm644  gnome-disk-utility.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  udisks2.mo /usr/share/locale-langpack/et/LC_MESSAGES
+sudo install -Dm644  gnome-disk-utility.mo /usr/share/locale-langpack/et/LC_MESSAGES
 exit 1
