3c004d831e1535177e521ea083451da7818bf846
vaba <92581921+vabaME@users.noreply.github.com>
2023-03-14 14:33:42 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -24,4 +24,5 @@ sudo install -Dm644  seahorse.mo /usr/share/locale-langpack/et/LC_MESSAGES/
 sudo install -Dm644  simple-scan.mo /usr/share/locale/et/LC_MESSAGES/
 sudo install -Dm644  simple-scan.desktop /usr/share/applications/
 sudo install -Dm644  psensor.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  warpinator.mo /usr/share/locale/et/LC_MESSAGES/
 exit 1
