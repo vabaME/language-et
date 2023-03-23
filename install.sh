e2dded1336986f9fbafd18772a794faace5383a4
vaba <92581921+vabaME@users.noreply.github.com>
2023-03-23 13:31:38 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -25,4 +25,5 @@ sudo install -Dm644  simple-scan.mo /usr/share/locale/et/LC_MESSAGES/
 sudo install -Dm644  simple-scan.desktop /usr/share/applications/
 sudo install -Dm644  psensor.mo /usr/share/locale/et/LC_MESSAGES/
 sudo install -Dm644  warpinator.mo /usr/share/locale/et/LC_MESSAGES/
+sudo install -Dm644  celluloid.mo /usr/share/locale/et/LC_MESSAGES/
 exit 1
